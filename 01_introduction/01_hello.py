ab25e708b3ea62ac8e8759ef660d67a1be93fb6b
AnsloneKritisch <digveshoptimus@gmail.com>
2025-02-01 20:27:18 +0530

    new

---
+++ 01_introduction/01_hello.py
@@ -0,0 +1,6 @@
+print(" hello World !!") 
+#print statment helps to print the output
+
+print("Hello" + " " + "World" )
+
+#the + sign is used to join the strings or to add variables
---
+++ 01_introduction/02_input.py
@@ -0,0 +1,20 @@
+#input function help to take input !
+input("What is your name ?")
+
+#it will print the message but won't store the data
+print("Hello " + input("What is your name ?"))
+
+#but we need to store the data somewhere right ???
+name = input("What is your name ?")
+print("Hello " + name)
+
+"""
+try to print the message with "!" at end 
+
+Output I need -> hello <input name> !
+
+Download the app from this website and paste the code there
+This website will explain you stepwise how code works
+https://thonny.org/
+
+"""
